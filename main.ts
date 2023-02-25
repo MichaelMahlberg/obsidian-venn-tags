1292a18536636f8a069326f80964ca9340a97ed4
Michael Mahlberg <mm@consulting-guild.de>
2023-02-25 16:34:27 +0100

    chore: Plugin naming now reflects intended use
    main.ts and manifest.json have been infused with the new knowledge

--- main.ts
+++ main.ts
@@ -2,16 +2,16 @@ import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Set
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface VenTagsSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: VenTagsSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class VennTags extends Plugin {
+	settings: VenTagsSettings;
 
 	async onload() {
 		await this.loadSettings();
@@ -108,9 +108,9 @@ class SampleModal extends Modal {
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: VennTags;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: VennTags) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
